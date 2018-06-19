ab0ad1bdc77b72cb2c8c30a896c9d0bd68a6e7d8
BcRikko <s.shin1106@gmail.com>
2018-06-20 08:48:55 +0900

    :sparkles: -mフラグの実装

--- ls/ls.go
+++ ls/ls.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
+)
+
+var (
+	m bool
 )
 
 func ls(dir string) {
@@ -12,18 +18,37 @@ func ls(dir string) {
 		return
 	}
 
+	list := []string{}
 	for _, fileInfo := range fileInfos {
-		fmt.Println(fileInfo.Name())
+		list = append(list, fileInfo.Name())
+	}
+
+	if m {
+		fmt.Println(strings.Join(list, ", "))
+	} else {
+		for _, file := range list {
+			fmt.Println(file)
+		}
+	}
+}
+
+func initFlag() {
+	flag.BoolVar(&m, "m", false, "separated by commas")
+}
+
+func getDir() string {
+	if flag.NArg() > 0 {
+		return flag.Arg(0)
+	} else {
+		dir, _ := os.Getwd()
+		return dir
 	}
 }
 
 func main() {
-	var dir string
-	if len(os.Args) > 1 {
-		dir = os.Args[1]
-	} else {
-		dir, _ = os.Getwd()
-	}
+	initFlag()
+	flag.Parse()
 
+	dir := getDir()
 	ls(dir)
 }
