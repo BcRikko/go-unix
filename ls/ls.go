e82bc1ef7fed6925158903b11ebdecdedebc8b20
BcRikko <s.shin1106@gmail.com>
2018-06-20 14:12:14 +0900

    :sparkles: -lフラグの実装

--- ls/ls.go
+++ ls/ls.go
@@ -11,8 +11,16 @@ import (
 var (
 	a bool
 	m bool
+	l bool
 	p bool
 )
+
+func initFlag() {
+	flag.BoolVar(&a, "a", false, "Include directory entries whose names begin with a dot (.).")
+	flag.BoolVar(&m, "m", false, "Stream output format; list files across the page, separated by commas.")
+	flag.BoolVar(&l, "l", false, "List in long format.")
+	flag.BoolVar(&p, "p", false, "Write a slash (`/') after each filename if that file is a directory.")
+}
 
 func ls(dir string) {
 	fileInfos, err := ioutil.ReadDir(dir)
@@ -22,17 +30,27 @@ func ls(dir string) {
 
 	list := []string{}
 	for _, fileInfo := range fileInfos {
-		file := fileInfo.Name()
-
-		if !a && strings.HasPrefix(file, ".") {
+		if !a && strings.HasPrefix(fileInfo.Name(), ".") {
 			continue
 		}
 
+		filename := fileInfo.Name()
 		if p && fileInfo.IsDir() {
-			file += "/"
+			filename += "/"
 		}
 
-		list = append(list, file)
+		if l && !m {
+			output := fmt.Sprintf(
+				"%v\t%10d\t%v\t%v",
+				fileInfo.Mode(),
+				fileInfo.Size(),
+				fileInfo.ModTime().Format("Jan _2 15:04"),
+				filename,
+			)
+			list = append(list, output)
+		} else {
+			list = append(list, filename)
+		}
 	}
 
 	if m {
@@ -42,12 +60,6 @@ func ls(dir string) {
 			fmt.Println(file)
 		}
 	}
-}
-
-func initFlag() {
-	flag.BoolVar(&a, "a", false, "Include directory entries whose names begin with a dot (.).")
-	flag.BoolVar(&m, "m", false, "Stream output format; list files across the page, separated by commas.")
-	flag.BoolVar(&p, "p", false, "Write a slash (`/') after each filename if that file is a directory.")
 }
 
 func getDir() string {
